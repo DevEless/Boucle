0d9fbee076c348aa26fef9fd7e82e243bd9b789f
DevEless <qdelameilleure@gmaile.com>
2022-12-22 09:33:39 +0100

    mmmh saucisse

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+console.log(Exo1)
+let mmhsaucisson = ["Quentin, Yassine, Adil , Charlotte, Enide, Bachbach, Kadrilatere, Denis, Brandon , Mohammed, Seb, Idriss, Fadia"]
+for (let i = 0; i < mmhsaucisson.length; i++) {
+    console.log(`Salut ça va ???????!!!!!????!!!!??? ${mmhsaucisson[i]}`)
+    
+}
+
