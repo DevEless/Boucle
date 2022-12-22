b6c049a5e5093e92724de0cf7a9770167520f05d
DevEless <qdelameilleure@gmaile.com>
2022-12-22 09:56:56 +0100

    mmmh saucisse

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,20 @@ for (let i = 0; i < mmhsaucisson.length; i++) {
     console.log(`Salut ça va ???????!!!!!????!!!!??? ${mmhsaucisson[i]}`)
     
 }
+console.log(Exo2)
+let nombre = prompt("INSERE UN CHIFFRE (jusque 20) !!!!!!!!!!!!!!!!!!!")
+for (let i = 0; i<=nombre ; i++) {
+    console.log(`$nombre[i]`) ;
+    
+}
 
+console.log(Exo3)
+let mmhsaucisson2= prompt("inserer nom -HODOR")
+mmhsaucisson2.forEach(element => {console.log(`element ${mmhsaucisson2}`)
+});
+
+console.log(Exo4)
+let array = ['Saucisse', 'Frite ', 'mmmh']
+array.forEach(element => {console.log(`${array}`)
+    
+});
